13dd788d0d590f3061e1d5f97dc63bf87cc17975
Alvaro Salgado <alvarosalgado@miido.co>
2016-03-13 18:00:29 -0500

    barcodescanner
    1. Inclusion de libreria de scaneo
    2. Implementacion de libreria de escaneo

--- app/src/main/java/gss/llavedelsaber/bcsActivity.java
+++ app/src/main/java/gss/llavedelsaber/bcsActivity.java
@@ -5,7 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class bcsActivity extends AppCompatActivity {
+import com.scandit.barcodepicker.BarcodePicker;
+import com.scandit.barcodepicker.OnScanListener;
+import com.scandit.barcodepicker.ScanSession;
+import com.scandit.barcodepicker.ScanSettings;
+import com.scandit.barcodepicker.ScanditLicense;
+import com.scandit.recognition.Barcode;
+
+
+/**
+ * @version 2.0
+ * @author Alvaro Fernando Salgado
+ * @serial ALALFELASAROMO
+ * Designed For Global System Solutions S.A.S
+ */
+public class bcsActivity extends AppCompatActivity implements OnScanListener {
+
+    private BarcodePicker picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +34,52 @@ public class bcsActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_bcs);
 
+        init_UI();
+        init_System();
+
+    }
+
+    /**
+     * Init all basics app user interface requirements
+     */
+    private void init_UI() {
+        setContentView(picker);
+    }
+
+    /**
+     * Init the basics app logical requirements
+     */
+    private void init_System() {
+        ScanditLicense.setAppKey(gss.llavedelsaber.system.constants.BAR_CODE_SCANNER_KEY);
+        ScanSettings settings = ScanSettings.create();
+        settings.setSymbologyEnabled(Barcode.SYMBOLOGY_EAN13, true);
+        settings.setSymbologyEnabled(Barcode.SYMBOLOGY_UPCA, true);
+        picker = new BarcodePicker(this, settings);
+        picker.setOnScanListener(this);
+    }
+
+
+
+    @Override
+    public void didScan(ScanSession scanSession) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        picker.stopScanning();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        picker.startScanning();
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        picker.stopScanning();
+        super.onDestroy();
     }
 }
